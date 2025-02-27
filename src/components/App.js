1aedabe3075540891cd78209f6ba3c0ab5ff77b9
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-27 21:47:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+const Home = () => <h1>Welcome to my website!</h1>;
+const About = () => <h1>This is a sample React Router program.</h1>;
 
 const App = () => {
-  return <h1>Welcome to my website!</h1>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
