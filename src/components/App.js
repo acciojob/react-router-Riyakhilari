f69674a156aa18e85a6b8a67ef6c40620cb30877
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-03 22:25:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-const Home = () => <h1>Welcome to my website!</h1>;
-const About = () => <h1>This is a sample React Router program.</h1>;
+
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter,Link, Route, Routes } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+       <li> <Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link> </li>
+    </ul>
+    </header>
+  );
+};
+
+
+const Home = () => {
+  return (
+    <h1>
+     Welcome to my website!
+    </h1>  
+  );
+};
+const About = () => {
+  return (
+    <div>
+    <h1>
+     About
+    </h1>
+    <p>This is a sample React Router program.</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
+   <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
-  );
-};
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
